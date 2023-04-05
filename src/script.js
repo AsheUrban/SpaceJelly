670728cce16edccf7e1eb5e4c49c6bad7a632c52
Ashe Urban <ashe@goldentongue.com>
2023-04-05 14:00:15 -0700

    WIP mousecontrols stillllllll.
    Co-authored-by: Jacob Maier <jacobamaier@gmail.com>
    Co-authored-by: Jackson Levine <jack@netbug.us>
    Co-authored-by: Vera Weikel <vera.weikel@gmail.com>
    Co-authored-by: Mesha Devan <madsketchz@gmail.com>

--- src/script.js
+++ src/script.js
@@ -1,7 +1,11 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+// Texture
+
+const loader = new THREE.TextureLoader();
+const starMap = loader.load("./textures/particles/star.png");
 
 // Canvas
 const canvas = document.querySelector("canvas");
@@ -9,134 +13,127 @@ const canvas = document.querySelector("canvas");
 // Scene
 const scene = new THREE.Scene();
 
-// Camera
-const camera = new THREE.PerspectiveCamera(
-  75, // FOV - Field of view
-  window.innerWidth / window.innerHeight, // Aspect ratio - might need to be innerHeight/innerHeight
-  0.1, // Near clipping distance limit
-  100000 // Far clipping distance limit
+// Objects
+const geometry = new THREE.SphereGeometry(1, 75, 40);
+const starsGeometry = new THREE.BufferGeometry();
+const starsCount = 1000;
+const positionArray = new Float32Array(starsCount * 3);
+
+for (let i=0; i < starsCount * 3; i++) {
+  positionArray[i] = (Math.random() - 0.5) * (Math.random() * 7) * (Math.random() * 1);
+}
+
+starsGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positionArray, 3)
 );
-camera.position.z = 3; // Pull the camera back a bit so you can see the object
-// scene.add(camera);
+
+// Materials
+
+const material = new THREE.PointsMaterial({
+  size: 0.008,
+  color: "#00F0FF",
+});
+
+const starsMaterial = new THREE.PointsMaterial({
+  size: 0.008,
+  map: starMap,
+  transparent: true,
+  color: "#ff0f00",
+});
+
+// Mesh
+const sphere = new THREE.Points(geometry, material);
+const starsMesh = new THREE.Points(starsGeometry, starsMaterial);
+scene.add(sphere, starsMesh);
 
 // Lights
-const ambientLight = new THREE.AmbientLight(0xffffff, 2);  // Create a basic white universal light  // TODO: Change this to a fancier light!
-ambientLight.position.set(0, 0, 0); // Set position of the light
-scene.add(ambientLight); 
-
-const pointLight = new THREE.PointLight(0xffffff, 2); // Create a point light
-pointLight.position.set(0, 0, 0);
+const pointLight = new THREE.PointLight(0xffffff, 0.8);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
 scene.add(pointLight);
+
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  // update camer
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  // update renderer
+  renderer.setSizes(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Camera
+
+// Base Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width/sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true; // use to give a sense of weight
-controls.enabled = true;
-
-// Particles
-const particlesGeometry = new THREE.BufferGeometry(); // Geometry for stars
-const particlesCount = 15000; // particles to be created. Is equiv to 5000 * 3 (x,y,z vertices)
-const vertices = new Float32Array(particlesCount); // float of 32 bits (from buffer geo - vertices arr[x, y, z])
-
-// loop through all arr[x,y,z] w for loop (rand position)
-for( let i = 0; i < particlesCount; i++)
-{
-  vertices[i] = (Math.random() - 0.5) * 150;// mult (Math.rand - 0.5 to +.5)by 100; Range -50 through +50
-}
-particlesGeometry.setAttribute(
-  "position", 
-  new THREE.BufferAttribute(vertices, 3) // stores data ie. vertices position, custom attributes// 3 vals [xyz] per docs
-)
-
-// Texture (loader fxn)
-const textureLoader = new THREE.TextureLoader();
-const particleTexture = textureLoader.load("/textures/particles/bubble.png"); // TODO // Adds particle textures
-
-// Material
-const particlesMaterial = new THREE.PointsMaterial({
-  map: particleTexture, // Texture
-  size: .6, // size of particles
-  sizeAttenuation: true, //bool// particle sz gets smaller (val:0-3) as the camera zooms out & vice versa
-  transparent: true,
-  depthWrite: false
-});
-
-const stars = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(stars);
-
-const gltfLoader = new GLTFLoader(); // Create a loader
-
-// make async loader
-const loadAsync = url => {
-  return new Promise(resolve => {
-    gltfLoader.load(url, gltf => {
-      resolve(gltf)
-    })
-  })
-}
-Promise.all( [loadAsync('./saturn/scene.gltf'), loadAsync('./sun/scene.gltf'), loadAsync('./jellyfish/scene.gltf')] ).then(models => {
-  // get what you need from the models array
-  const saturn = models[0].scene.children[0]
-    saturn.position.set(0, 0, 0);
-    saturn.scale.set(0.001, 0.001, 0.001);
-  const sun = models[1].scene.children[0]
-    sun.position.set(-15, 0, 0);
-    sun.scale.set(0.1, 0.1, 0.1);
-  const jf = models[2].scene.children[0]
-    jf.position.set(5, 0, 0);
-    jf.scale.set(0.01, 0.01, 0.01);
-
-    console.log("saturn", saturn);
-    console.log("sun", sun);
-  // add both models to the scene
-  scene.add(saturn)
-  scene.add(sun)
-  scene.add(jf)
-})
+controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas, // Canvas is the canvas element from html
-  alpha: true
+  canvas: canvas,
 });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor(new THREE.Color("#8c8c8c"), 0.7);
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // Avoid pixelation on high res screens
+// Mouse Movement
 
+document.addEventListener("mousemove", animateStars);
+let mouseX = 0;
+let mouseY = 0;
 
-const mouse = new THREE.Vector3();
-const cameraPosition = new THREE.Vector3(50, 50, 50);
+function animateStars(event) {
 
-function onMouseMove(event) {
-  mouse.x = (event.clientX / window.innerWidth) * 5 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 5 + 1;
+  mouseY = event.ClientY;
+  mouseX = event.clientX;
 }
 
-function updateCameraPosition() {
-  cameraPosition.x += (mouse.x - cameraPosition.x) * 0.45;
-  cameraPosition.y += (-mouse.y - cameraPosition.y) * 0.45;
-
-  camera.position.copy(cameraPosition);
-  camera.lookAt(scene.position);
-}
-
-canvas.addEventListener("mousemove", onMouseMove);
 // Animate
-const animate = () => {
-    // Update the controls
-    controls.update();
 
-    // Rotate the stars a bit, frame by frame
-    stars.rotation.y -= 0.001;
+const clock = new THREE.Clock();
 
-    updateCameraPosition();
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    //Render the scene
-    renderer.render(scene, camera);
+  // Update objects
+  sphere.rotation.y = 0.15 * elapsedTime;
+  starsMesh.rotation.x = -0.1 * elapsedTime;
 
-    //Calls itself next frame, to repeat
-    requestAnimationFrame(animate);
+  if (mouseX > 0) {
+    sphere.rotation.y = mouseX * (elapsedTime * -0.00008);
+    starsMesh.rotation.y = mouseY * (elapsedTime * 0.0002);
+  }
+
+  // Update Orbital Controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the frame
+  window.requestAnimationFrame(tick);
 };
 
-animate();
-
+tick();
